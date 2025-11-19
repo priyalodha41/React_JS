64b36795e7a2fffd74aacbc5c1182f98babb132c
priyalodha41 <priyalodha41@gmail.com>
2025-11-19 18:02:44 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/React_JS/",   // 👈 IMPORTANT: add repo name here
   plugins: [react()],
 })
